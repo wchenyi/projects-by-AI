e3f17e0aec8bbefe4e6d098caaba72fdad914c07
Wangcy <93115719+wchenyi@users.noreply.github.com>
2025-08-11 17:15:46 +0800

    Update data.js

---
+++ appstore/apps/linux/data.js
@@ -0,0 +1,268 @@
+// apps/linux/data.js
+
+export const linuxApps = [
+  // --- Large Cards ---
+  {
+    name: "Nekoray",
+    category: "代理工具",
+    icon: "apps/linux/img/Nekoray.png",
+    size: "large",
+    rating: "5.0",
+    platform: "Linux",
+    downloads: {
+      github: "https://github.com/MatsuriDayo/nekoray/releases"
+    }
+  },
+  {
+    name: "ClashMeta",
+    category: "代理工具",
+    icon: "apps/linux/img/ClashMeta.png",
+    size: "large",
+    rating: "5.0",
+    platform: "Linux",
+    downloads: {
+      github: "https://github.com/MetaCubeX/Clash.Meta/releases"
+    }
+  },
+  {
+    name: "clash-rev",
+    category: "代理工具",
+    icon: "apps/linux/img/clash-rev.png",
+    size: "large",
+    rating: "5.0",
+    platform: "Linux",
+    downloads: {
+      github: "https://github.com/MerlinKodo/clash-rev"
+    }
+  },
+  {
+    name: "clash-verge-rev",
+    category: "代理工具",
+    icon: "apps/linux/img/clash-verge-rev.png",
+    size: "large",
+    rating: "5.0",
+    platform: "Linux",
+    downloads: {
+      github: "https://github.com/clash-verge-rev/clash-verge-rev"
+    }
+  },
+  {
+    name: "Mihomo Party",
+    category: "代理工具",
+    icon: "apps/linux/img/MihomoParty.png",
+    size: "large",
+    rating: "5.0",
+    platform: "Linux",
+    downloads: {
+      github: "https://github.com/pompurin404/mihomo-party/releases"
+    }
+  },
+  {
+    name: "Hiddify-next",
+    category: "代理工具",
+    icon: "apps/linux/img/Hiddify-next.png",
+    size: "large",
+    rating: "5.0",
+    platform: "Linux",
+    downloads: {
+      github: "https://github.com/hiddify/hiddify-next/releases"
+    }
+  },
+  {
+    name: "sing-box",
+    category: "代理工具",
+    icon: "apps/linux/img/sing-box.png",
+    size: "large",
+    rating: "5.0",
+    platform: "Linux",
+    downloads: {
+      github: "https://github.com/SagerNet/sing-box"
+    }
+  },
+  {
+    name: "F1Clash",
+    category: "代理工具",
+    icon: "apps/linux/img/F1Clash.png",
+    size: "large",
+    rating: "5.0",
+    platform: "Linux",
+    downloads: {
+      github: "https://github.com/chen08209/FlClash"
+    }
+  },
+
+  // --- Medium Cards ---
+  {
+    name: "Qv2ray",
+    category: "代理工具",
+    icon: "apps/linux/img/Qv2ray.png",
+    size: "medium",
+    rating: "4.5",
+    platform: "Linux",
+    downloads: {
+      github: "https://github.com/Qv2ray/Qv2ray"
+    }
+  },
+  {
+    name: "v2rayL",
+    category: "代理工具",
+    icon: "apps/linux/img/v2rayL.png",
+    size: "medium",
+    rating: "4.5",
+    platform: "Linux",
+    downloads: {
+      github: "https://github.com/jiangxufeng/v2rayL/releases"
+    }
+  },
+  {
+    name: "Mellow",
+    category: "代理工具",
+    icon: "apps/linux/img/Mellow.png",
+    size: "medium",
+    rating: "4.5",
+    platform: "Linux",
+    downloads: {
+      github: "https://github.com/mellow-io/mellow"
+    }
+  },
+  {
+    name: "Clashy",
+    category: "代理工具",
+    icon: "apps/linux/img/Clashy.png",
+    size: "medium",
+    rating: "4.5",
+    platform: "Linux",
+    downloads: {
+      github: "https://github.com/SpongeNobody/Clashy/releases"
+    }
+  },
+  {
+    name: "Outline",
+    category: "代理工具",
+    icon: "apps/linux/img/Outline.png",
+    size: "medium",
+    rating: "4.5",
+    platform: "Linux",
+    downloads: {
+      official: "https://getoutline.org/zh-CN/"
+    }
+  },
+  {
+    name: "Trojan-qt5",
+    category: "代理工具",
+    icon: "apps/linux/img/Trojan-qt5.png",
+    size: "medium",
+    rating: "4.5",
+    platform: "Linux",
+    downloads: {}
+  },
+  {
+    name: "Clash Mutiplatform",
+    category: "代理工具",
+    icon: "apps/linux/img/ClashMutiplatform.png",
+    size: "medium",
+    rating: "4.5",
+    platform: "Linux",
+    downloads: {
+      alternative: "https://t.me/c/1834195052/41"
+    }
+  },
+
+  // --- Small Cards ---
+  {
+    name: "v2rayA",
+    category: "代理工具",
+    icon: "apps/linux/img/v2rayA.png",
+    size: "small",
+    rating: "4.0",
+    platform: "Linux",
+    description: "Trojan客户端",
+    downloads: {
+      github: "https://github.com/v2rayA/v2rayA/releases"
+    }
+  },
+  {
+    name: "Trojan-Qt5",
+    category: "代理工具",
+    icon: "apps/linux/img/Trojan-Qt5.png",
+    size: "small",
+    rating: "4.0",
+    platform: "Linux",
+    description: "Trojan客户端",
+    downloads: {
+      official: "https://itlanyan.com/download.php?filename=/trojan/Trojan-Qt5-v1.4.0.7z"
+    }
+  },
+  {
+    name: "OpenWrt",
+    category: "路由器",
+    icon: "apps/linux/img/OpenWrt.png",
+    size: "small",
+    rating: "4.0",
+    platform: "Linux",
+    description: "Trojan客户端",
+    downloads: {
+      official: "https://openwrt.org/"
+    }
+  },
+  {
+    name: "Merlin",
+    category: "路由器",
+    icon: "apps/linux/img/Merlin.png",
+    size: "small",
+    rating: "4.0",
+    platform: "Linux",
+    description: "Trojan客户端",
+    downloads: {
+      official: "https://www.koolcenter.com/category/merlin"
+    }
+  },
+  {
+    name: "OpenWrt",
+    category: "路由器",
+    icon: "apps/linux/img/OpenWrt.png",
+    size: "small",
+    rating: "4.0",
+    platform: "Linux",
+    description: "路由器",
+    downloads: {
+      official: "https://openwrt.org/"
+    }
+  },
+  {
+    name: "Merlin",
+    category: "路由器",
+    icon: "apps/linux/img/Merlin.png",
+    size: "small",
+    rating: "4.0",
+    platform: "Linux",
+    description: "路由器",
+    downloads: {
+      official: "https://www.koolcenter.com/category/merlin"
+    }
+  },
+  {
+    name: "Tuic",
+    category: "协议/内核",
+    icon: "apps/linux/img/Tuic.png",
+    size: "small",
+    rating: "4.0",
+    platform: "Linux",
+    description: "协议、内核",
+    downloads: {
+      github: "https://github.com/EAimTY/tuic/tree/dev"
+    }
+  },
+  {
+    name: "hysteria",
+    category: "协议/内核",
+    icon: "apps/linux/img/hysteria.png",
+    size: "small",
+    rating: "4.0",
+    platform: "Linux",
+    description: "协议、内核",
+    downloads: {
+      github: "https://github.com/apernet/hysteria"
+    }
+  }
+];
