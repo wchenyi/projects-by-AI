5ee9820ed978a7b33cdcc2ad9629f64d10fc7d9e
Wangcy <93115719+wchenyi@users.noreply.github.com>
2025-08-11 12:19:27 +0800

    Create  sw.js

---
+++ appstore/sw.js/sw.js
@@ -0,0 +1,8 @@
+// sw.js
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installing.');
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
