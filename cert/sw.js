6cb9df87eb6751b6bf00add965983a3769e4e03c
Wcc <93115719+wchenyi@users.noreply.github.com>
2026-06-26 22:13:37 +0800

    Add files via upload

---
+++ cert/sw.js
@@ -0,0 +1,4 @@
+const CACHE_NAME = 'award-editor-v1';
+const urlsToCache = ['./index.html'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
